e1e96ffe8a51a1fc7ce315c52899f872f7b0873d
Vikas Choubey <vikas.cby@gmail.com>
2019-06-19 14:36:53 +0530

    adding files for music websapp

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
-  const visual = document.querySelector(".visual");
-  const colors = [
+  const visual = document.querySelector(".visual")
+  const colours = [
     "#60d394",
-    "#d36060",
-    "#c060d3",
-    "#d3d160",
-    "#606bd3",
-    "#60c2d3"
-  ];
-
+     "#d36060",
+      "#c060d3", 
+      "#d3d160", 
+      "#6860d3", 
+      "#60b2d3"
+    ];
+//   play sounds
+  
   pads.forEach((pad, index) => {
-    pad.addEventListener("click", function() {
+    pad.addEventListener('click', function(){
       sounds[index].currentTime = 0;
       sounds[index].play();
-      createBubble(index);
+      createBubbles(index);
     });
   });
 
-  const createBubble = index => {
-    //Create bubbles
+  //create bubbles
+
+  const createBubbles = (index) => {
     const bubble = document.createElement("div");
     visual.appendChild(bubble);
-    bubble.style.backgroundColor = colors[index];
+    bubble.style.backgroundColor = colours[index];
     bubble.style.animation = `jump 1s ease`;
-    bubble.addEventListener("animationend", function() {
+    bubble.addEventListener("animationend", function(){
       visual.removeChild(this);
     });
   };
+
 });
+
+
